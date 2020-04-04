008124e7ecb7f2bd851424d077fb16e5aab6c51d
ahao214 <haochen214@163.com>
2020-04-04 17:41:43 +0800

    简单句子结点类

---
+++ InterpreterSample/SentenceNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterSample
+{
+    /// <summary>
+    /// 简单句子结点类，充当非终结符表达式角色
+    /// </summary>
+    class SentenceNode:AbstractNode
+    {
+        private AbstractNode direction;
+        private AbstractNode action;
+        private AbstractNode distance;
+
+        public SentenceNode(AbstractNode direction,AbstractNode action ,AbstractNode distance)
+        {
+            this.direction = direction;
+            this.action = action;
+            this.distance = distance;
+        }
+
+        //简单句子的解释操作
+        public override string Interpret()
+        {
+            return direction.Interpret() + action.Interpret() + distance.Interpret();
+        }
+    }
+}
