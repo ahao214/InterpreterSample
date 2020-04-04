b517b30140bfb527e6fe90674422971e61b19a92
ahao214 <haochen214@163.com>
2020-04-04 17:49:41 +0800

    距离和动作结点类

---
+++ InterpreterSample/ActionNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterSample
+{
+    /// <summary>
+    /// 动作结点类，充当终结符表达式角色
+    /// </summary>
+    class ActionNode:AbstractNode
+    {
+        private string action;
+        public ActionNode (string action)
+        {
+            this.action = action;
+        }
+
+        //动作（移动方式）表达式的解释操作
+        public override string Interpret()
+        {
+            if (action.Equals("move"))
+            {
+                return "移动";
+            }
+            else if(action.Equals ("run"))
+            {
+                return "快速移动";
+            }
+            else
+            {
+                return "无效指令";
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ InterpreterSample/DistanceNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterSample
+{
+    /// <summary>
+    /// 距离结点类，充当终结符表达式角色
+    /// </summary>
+    class DistanceNode:AbstractNode
+    {
+        private string distance;
+        public DistanceNode(string distance)
+        {
+            this.distance = distance;
+        }
+
+        //距离表达式的解释操作
+        public override string Interpret()
+        {
+            return this.distance;
+        }
+    }
+}
