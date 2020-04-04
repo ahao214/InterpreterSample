d845a8765bbaea0aa4a1a55f8dc491da70533457
ahao214 <haochen214@163.com>
2020-04-04 17:26:17 +0800

    And结点类

---
+++ InterpreterSample/AddNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace InterpreterSample
+{
+    /// <summary>
+    /// And结点类，充当非终结符表达式角色
+    /// </summary>
+    public class AddNode:AbstractNode
+    {
+        public AddNode()
+        {
+        }
+
+        private AbstractNode left;  //And的左表达式
+        private AbstractNode right; //And的右表达式
+        
+
+        public AddNode(AbstractNode left,AbstractNode right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        //And表达式解释操作
+        public override string Interpret()
+        {
+            return left.Interpret() + "再" + right.Interpret();
+        }
+    }
+}
