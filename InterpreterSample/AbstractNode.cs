9ef047e737f64334b75c296d02c19e13d0ef358a
ahao214 <haochen214@163.com>
2020-04-04 17:11:33 +0800

    解释器模式

---
+++ InterpreterSample/AbstractNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterSample
+{
+    /// <summary>
+    /// 抽象结点类，充当抽象表达式角色
+    /// </summary>
+    abstract class AbstractNode
+    {
+        public abstract string Interpret();
+    }
+}
---
+++ InterpreterSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterSample
+{
+    /// <summary>
+    /// 解释器模式
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
